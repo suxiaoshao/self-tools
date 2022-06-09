0a2dc8c300ef5f90ca4f692e5a390cdd92125ef5
sushao <suxiaoshao@gmial.com>
2022-06-09 18:59:28 +0800

    refactor(server): login set-cookie -> auth

--- server/packages/login/src/router/login.rs
+++ server/packages/login/src/router/login.rs
@@ -1,14 +1,11 @@
-use axum::{
-    http::{header::SET_COOKIE, HeaderMap, HeaderValue},
-    Json,
-};
+use axum::Json;
 use proto::{
     auth::{LoginReply, LoginRequest},
     middleware::client::login_client,
 };
 use serde::Deserialize;
 
-use crate::errors::OpenResult;
+use crate::errors::{response::OpenResponse, OpenResult};
 #[derive(Deserialize, Debug)]
 pub struct LoginInput {
     username: String,
@@ -16,14 +13,11 @@ pub struct LoginInput {
 }
 pub(crate) async fn login(
     Json(LoginInput { username, password }): Json<LoginInput>,
-) -> OpenResult<HeaderMap> {
+) -> OpenResult<OpenResponse<String>> {
     let mut client = login_client(None).await?;
     let LoginReply { auth } = client
         .login(LoginRequest { username, password })
         .await?
         .into_inner();
-    let mut headers = HeaderMap::new();
-    let set_cookie = HeaderValue::from_str(&format!("auth={auth}; domain=.sushao.top"))?;
-    headers.insert(SET_COOKIE, set_cookie);
-    Ok(headers)
+    Ok(OpenResponse::new(auth))
 }
