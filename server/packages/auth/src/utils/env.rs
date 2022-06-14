27308be336e48f6c10f32673b90a699f92f22455
sushao <suxiaoshao@gmial.com>
2022-06-14 23:02:08 +0800

    refactor(server): bookmarks dotenv -> std::env

--- server/packages/auth/src/utils/env.rs
+++ server/packages/auth/src/utils/env.rs
@@ -1,12 +1,14 @@
+use std::env;
+
 use errors::{TonicError, TonicResult};
 
 pub fn env_password() -> TonicResult<String> {
-    dotenv::var("PASSWORD").map_err(|_| TonicError::PasswordNotSet)
+    env::var("PASSWORD").map_err(|_| TonicError::PasswordNotSet)
 }
 
 pub fn env_secret_key() -> TonicResult<String> {
-    dotenv::var("SECRET").map_err(|_| TonicError::SecretKeyNotSet)
+    env::var("SECRET").map_err(|_| TonicError::SecretKeyNotSet)
 }
 pub fn env_username() -> TonicResult<String> {
-    dotenv::var("USERNAME").map_err(|_| TonicError::UsernameNotSet)
+    env::var("USERNAME").map_err(|_| TonicError::UsernameNotSet)
 }
