4b5e332a8ccf37b25e5692439b049c65e2e60666
suxiaoshao <suxiaoshao@gamil.com>
2022-10-31 00:30:36 +0800

    feat(bookmarks): theme material you color

---
+++ web/common/theme/src/genTheme.ts
@@ -0,0 +1,43 @@
+import { argbFromHex, themeFromSourceColor, Theme as YouTheme, hexFromArgb } from '@material/material-color-utilities';
+import { createTheme, Theme as MuiTheme } from '@mui/material';
+
+// Get the theme from a hex color
+export const genTheme = themeFromSourceColor(argbFromHex('#3c5ba9'));
+
+export function youThemeToMuiTheme(youTheme: YouTheme, mode: 'dark' | 'light' = 'light'): MuiTheme {
+  const theme = youTheme.schemes[mode];
+  return createTheme({
+    palette: {
+      mode,
+      primary: {
+        main: hexFromArgb(theme.primary),
+        contrastText: hexFromArgb(theme.onPrimary),
+      },
+      secondary: {
+        main: hexFromArgb(theme.secondary),
+        contrastText: hexFromArgb(theme.onSecondary),
+      },
+      error: {
+        main: hexFromArgb(theme.error),
+        contrastText: hexFromArgb(theme.onError),
+      },
+      warning: {
+        main: hexFromArgb(theme.tertiary),
+        contrastText: hexFromArgb(theme.onTertiary),
+      },
+      info: {
+        main: hexFromArgb(theme.tertiary),
+        contrastText: hexFromArgb(theme.onTertiary),
+      },
+      background: {
+        default: hexFromArgb(theme.background),
+        paper: hexFromArgb(theme.surface),
+      },
+      text: {
+        primary: hexFromArgb(theme.onBackground),
+        secondary: hexFromArgb(theme.onSurface),
+        disabled: hexFromArgb(theme.onSurfaceVariant),
+      },
+    },
+  });
+}
--- web/common/theme/src/index.tsx
+++ web/common/theme/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
+import { CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css';
+import { genTheme, youThemeToMuiTheme } from './genTheme';
 
 export interface CustomThemeProps {
   children?: React.ReactNode;
@@ -14,13 +15,7 @@ export function CustomTheme({ children }: CustomThemeProps): JSX.Element {
   const isDark = useMediaQuery('(prefers-color-scheme: dark)');
 
   const theme = React.useMemo(() => {
-    const newTheme = createTheme({
-      palette: isDark
-        ? {
-            mode: 'dark',
-          }
-        : undefined,
-    });
+    const newTheme = youThemeToMuiTheme(genTheme, isDark ? 'dark' : 'light');
     return newTheme;
   }, [isDark]);
   return (
