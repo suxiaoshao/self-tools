47b1202d018370c7949308f5cf2500a498a8f6fe
suxiaoshao <suxiaoshao@gamil.com>
2023-01-14 21:32:52 +0800

    chore(github): add github issue template

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+docker build -f ./docker/packages/collections.Dockerfile -t suxiaoshao/collections .
+docker build -f ./docker/packages/auth.Dockerfile -t suxiaoshao/auth .
+docker build -f ./docker/packages/login.Dockerfile -t suxiaoshao/login .
+docker build -f ./docker/packages/bookmarks.Dockerfile -t suxiaoshao/bookmarks .
