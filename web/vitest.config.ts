51c7aa7ba2db805f9cf4b0537f296246ca92b7da
Su Weijie <weijie.su@shopee.com>
2023-03-01 18:04:57 +0800

    test(others): 💍 add notify test

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    setupFiles: ['./web/config/test/testSetup.ts'],
+    coverage: {
+      reporter: ['text'],
+      provider: 'c8',
+    },
   },
 });
