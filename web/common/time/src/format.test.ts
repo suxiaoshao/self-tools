4b11e3e19a7b2e3a09a8c10b63140b079bd5d112
suxiaoshao <suxiaoshao@gamil.com>
2023-03-06 01:15:49 +0800

    test(all): 💍 update test TZ

--- web/common/time/src/format.test.ts
+++ web/common/time/src/format.test.ts
@@ -3,10 +3,10 @@ import { format } from './format';
 
 describe('format', () => {
   it('format time timestamp', () => {
-    expect(format(1677554648753)).toBe('2023-02-28 11:24:08');
+    expect(format(1677554648753)).toBe('2023-02-28 03:24:08');
   });
   it('format time string', () => {
-    expect(format('2023-01-19T19:04:15.759225Z')).toBe('2023-01-20 03:04:15');
+    expect(format('2023-01-19T19:04:15.759225Z')).toBe('2023-01-19 19:04:15');
   });
   it('format time empty', () => {
     expect(format(null)).toBe('~');
