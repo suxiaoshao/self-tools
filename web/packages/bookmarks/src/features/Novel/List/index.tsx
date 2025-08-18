97a5127879daf679d4ab9c62a8048d7531e12c16
suxiaoshao <48886207+suxiaoshao@users.noreply.github.com>
2025-08-19 04:46:13 +0800

    feat(bookmarks): 🎸 Refactor table columns to use createCustomColumnHelper

--- web/packages/bookmarks/src/features/Novel/List/index.tsx
+++ web/packages/bookmarks/src/features/Novel/List/index.tsx
@@ -1,6 +1,7 @@
 import { Refresh } from '@mui/icons-material';
 import { Avatar, Box, FormControl, FormLabel, IconButton, Link, Paper, Switch } from '@mui/material';
 import {
+  createCustomColumnHelper,
   type CustomColumnDefArray,
   CustomTable,
   type CustomTableOptions,
@@ -28,6 +29,8 @@ import TagsSelect from '@bookmarks/components/TagsSelect';
 
 type Data = GetNovelsQuery['queryNovels'][0];
 
+const columnHelper = createCustomColumnHelper<Data>();
+
 export default function NovelList() {
   type FormData = GetNovelsQueryVariables;
   const { control, watch, register } = useForm<FormData>({ defaultValues: { tagMatch: { matchSet: [] } } });
@@ -36,74 +39,73 @@ export default function NovelList() {
   const [deleteNovel] = useDeleteNovelMutation();
   const t = useI18n();
   const columns = useMemo<CustomColumnDefArray<Data>>(
-    () => [
-      {
-        header: t('name'),
-        id: 'name',
-        accessorFn: ({ id, name }) => (
-          <Link to={`/bookmarks/novel/${id}`} underline="hover" component={RouterLink}>
-            {name}
-          </Link>
-        ),
-        cell: (context) => context.getValue(),
-      },
-      {
-        header: t('novel_site'),
-        id: 'site',
-        accessorFn: ({ site }) => t(getLabelKeyBySite(site)),
-        cell: (context) => context.getValue(),
-      },
-      {
-        header: t('avatar'),
-        id: 'avatar',
-        accessorFn: ({ avatar }) => <Avatar src={getImageUrl(avatar)} />,
-        cell: (context) => context.getValue(),
-      },
-      {
-        header: t('description'),
-        id: 'description',
-        accessorFn: ({ description }) => description ?? '-',
-        cellProps: {
-          align: 'center',
-          sx: {
-            maxWidth: 700,
+    () =>
+      [
+        columnHelper.accessor(
+          ({ id, name }) => (
+            <Link to={`/bookmarks/novel/${id}`} underline="hover" component={RouterLink}>
+              {name}
+            </Link>
+          ),
+          {
+            header: t('name'),
+            id: 'name',
+            cell: (context) => context.getValue(),
           },
-        },
-        cell: (context) => context.getValue(),
-      },
-      {
-        header: t('create_time'),
-        id: 'createTime',
-        accessorFn: ({ createTime }) => format(createTime),
-        cell: (context) => context.getValue(),
-      },
-      {
-        header: t('update_time'),
-        id: 'updateTime',
-        accessorFn: ({ updateTime }) => format(updateTime),
-        cell: (context) => context.getValue(),
-      },
-      {
-        header: t('actions'),
-        id: 'action',
-        accessorFn: ({ id }) => (
-          <TableActions>
-            {(onClose) => [
-              {
-                text: t('delete'),
-                onClick: async () => {
-                  await deleteNovel({ variables: { id } });
-                  onClose();
-                  await refetch();
-                },
-              },
-            ]}
-          </TableActions>
         ),
-        cellProps: { padding: 'none' },
-        cell: (context) => context.getValue(),
-      },
-    ],
+        columnHelper.accessor(({ site }) => t(getLabelKeyBySite(site)), {
+          header: t('novel_site'),
+          id: 'site',
+          cell: (context) => context.getValue(),
+        }),
+        columnHelper.accessor(({ avatar }) => <Avatar src={getImageUrl(avatar)} />, {
+          header: t('avatar'),
+          id: 'avatar',
+          cell: (context) => context.getValue(),
+        }),
+        columnHelper.accessor(({ description }) => description ?? '-', {
+          header: t('description'),
+          id: 'description',
+          cellProps: {
+            align: 'center',
+            sx: {
+              maxWidth: 700,
+            },
+          },
+          cell: (context) => context.getValue(),
+        }),
+        columnHelper.accessor(({ createTime }) => format(createTime), {
+          header: t('create_time'),
+          id: 'createTime',
+          cell: (context) => context.getValue(),
+        }),
+        columnHelper.accessor(({ updateTime }) => format(updateTime), {
+          header: t('update_time'),
+          id: 'updateTime',
+          cell: (context) => context.getValue(),
+        }),
+        columnHelper.accessor(
+          ({ id }) => (
+            <TableActions>
+              {(onClose) => [
+                {
+                  text: t('delete'),
+                  onClick: async () => {
+                    await deleteNovel({ variables: { id } });
+                    onClose();
+                    await refetch();
+                  },
+                },
+              ]}
+            </TableActions>
+          ),
+          {
+            header: t('actions'),
+            id: 'action',
+            cell: (context) => context.getValue(),
+          },
+        ),
+      ] as CustomColumnDefArray<Data>,
     [deleteNovel, refetch, t],
   );
   const tableOptions = useMemo<CustomTableOptions<Data>>(
